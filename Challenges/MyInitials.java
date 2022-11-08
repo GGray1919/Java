217f8a6565b2a919d8165c9c49407942e5ba3ae5
GGray1919 <97263892+GGray1919@users.noreply.github.com>
2022-11-08 13:15:12 -0500

    Create MyInitials.java

---
+++ Challenges/MyInitials.java
@@ -0,0 +1,14 @@
+
+public class MyInitials {
+  public static void main(String[] args) {
+
+    System.out.println("BBBBBBBBB      GGGGGGG");
+    System.out.println("B        B   G");
+    System.out.println("B        B  G");
+    System.out.println("BBBBBBBbb   G");
+    System.out.println("B        B  G      GGGGGG");
+    System.out.println("B        B   G         G");
+    System.out.println("BBBBBBBBB      GGGGGGG");
+    
+  }
+}
